e6a96217cf39fd61a8b4a5d4133603b6656dd903
vidhyapkishore <vidhyapkishore@gmail.com>
2024-03-07 17:18:49 +0530

    Banner skelton added

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "./Components/NavBar/NavBar";
+import Banner from "./Components/Banner/Banner";
 import "./App.css"
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Banner/>
     </div>
   );
 }
