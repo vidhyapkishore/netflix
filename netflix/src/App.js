c467a57f32fddccf14519026a0debba5e1160720
vidhyapkishore <vidhyapkishore@gmail.com>
2024-03-07 16:51:06 +0530

    Navbar created

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,7 +1,10 @@
-
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar";
+import "./App.css"
 function App() {
   return (
     <div className="App">
+      <NavBar/>
     </div>
   );
 }
