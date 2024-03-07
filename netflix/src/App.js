a10173e5cecf5d7870f053f4d7f5c26de7dc7742
vidhyapkishore <vidhyapkishore@gmail.com>
2024-03-07 17:52:53 +0530

    Posters added and a basic design is finished

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import NavBar from "./Components/NavBar/NavBar";
 import Banner from "./Components/Banner/Banner";
 import "./App.css"
+import RowPost from "./Components/RowPost/RowPost";
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Banner/>
+      <RowPost/>
     </div>
   );
 }
