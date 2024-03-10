6ca90f047031d2076da8aac17e99924059ec1178
vidhyapkishore <vidhyapkishore@gmail.com>
2024-03-10 11:37:23 +0530

    Automated banner

--- netflix/src/Components/Banner/Banner.js
+++ netflix/src/Components/Banner/Banner.js
@@ -1,22 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import {API_KEY,imageUrl} from '../../constants/constants'
+import axios from '../../axios'
 import './Banner.css'
 function Banner() {
+  const [movie, setMovie] = useState();
+  const [randomNumber, setRandomNumber] = useState(0)
   useEffect(() => {
-         
+         axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then(
+          (response)=>{
+            // setRandomNumber(Math.floor(Math.random() * 10));
+            // console.log(Math.floor(Math.random() * 10));
+            // console.log(response.data.results[0]);
+            setMovie(response.data.results[Math.floor(Math.random() * 10)]);            
+            console.log(Math.floor(Math.random() * 100));
+          }
+         )
   }, [])
   
   return (
-    <div className='banner'>
+    <div
+    style={{backgroundImage:`url(${movie ? imageUrl+movie.backdrop_path : ''})`}}
+    className='banner'>
         <div className='content'>
-            <h1 className='title'>Movie Name</h1>
+            <h1 className='title'>{movie? movie.title : ''}</h1>
             <div className='banner_buttons'>
                 <button className='button'>Play</button>
                 <button className='button'>My List</button>
             </div>
-            <h1 className='description'>Lorem Ipsum is simply dummy text of the printing
-             and typesetting industry. Lorem Ipsum has been the industry's standard dummy 
-             text ever since the 1500s, when an unknown printer took a galley of type and 
-             scrambled it to make a type specimen book.</h1>
+            <h1 className='description'>
+              {movie? movie.overview : ''}
+            </h1>
         </div>
         <div className="fade_bottom"></div>
     </div>
